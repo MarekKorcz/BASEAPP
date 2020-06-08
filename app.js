c92a452a4a95bf05e56465d2f5b5b0a32f9ff1b8
Mark <mark.korcz@gmail.com>
2020-06-08 08:40:57 +0200

    docker finally run

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-// pushes variable from .env file to process.env
-// const dotenv = require('dotenv')
-// dotenv.config()
-
 // init environment variable
 const {
-    DB_NAME, DOMAIN_NAME, DB_DOMAIN_PORT, DB_APP_NAME, APP_PORT, HOST, HOST_USER, HOST_PASS, HOST_PORT
+    DB_NAME, DOMAIN_NAME, DB_DOMAIN_PORT, DB_APP_NAME, APP_PORT
 } = process.env
-
-console.log({DB_NAME, DOMAIN_NAME, DB_DOMAIN_PORT, DB_APP_NAME, APP_PORT, HOST, HOST_USER, HOST_PASS, HOST_PORT})
 
 const path = require('path')
 
@@ -50,38 +44,38 @@ const servers = require('./routes/servers')
 app.use('/server', servers)
 
 // get dynamic routings (like fetched ones)
-app.use(express.json({ limit: '1mb' }))
-app.post('/send-request-to-server', (req, res) => {
+// app.use(express.json({ limit: '1mb' }))
+// app.post('/send-request-to-server', (req, res) => {
 
-    var data = {
-        serverMessage: 'Previous command did not return false',
-        type: "error"
-    }
+//     var data = {
+//         serverMessage: 'Previous command did not return false',
+//         type: "error"
+//     }
 
-    // switch it later with DB request where you will get server datas by serverName...
-    // let serverData = req.body.serverName
+//     // switch it later with DB request where you will get server datas by serverName...
+//     // let serverData = req.body.serverName
 
-    var SSH = require('simple-ssh')
+//     var SSH = require('simple-ssh')
  
-    var ssh = new SSH({
-        host: HOST,
-        user: HOST_USER,
-        pass: HOST_PASS,
-        port: HOST_PORT
-    })
+//     var ssh = new SSH({
+//         host: HOST,
+//         user: HOST_USER,
+//         pass: HOST_PASS,
+//         port: HOST_PORT
+//     })
 
-    ssh
-        // !!!!! begin from here after create RemoteApp and test scripts with timeout !!!!!
-        .exec('cd innuendo; ls; exit 0;', {
-            out: (stdout) => {
+//     ssh
+//         // !!!!! begin from here after create RemoteApp and test scripts with timeout !!!!!
+//         .exec('cd innuendo; ls; exit 0;', {
+//             out: (stdout) => {
 
-                data.serverMessage = stdout
-                data.type = "success"
+//                 data.serverMessage = stdout
+//                 data.type = "success"
 
-                res.json(data)
-            }
-        })
-        .start()
-})
+//                 res.json(data)
+//             }
+//         })
+//         .start()
+// })
 
 app.listen(APP_PORT, () => console.log(`Server running on port ${APP_PORT}`))

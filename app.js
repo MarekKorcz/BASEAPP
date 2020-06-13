4da3847d6f677e8d2793d528e46eae3eea17d90d
Mark <mark.korcz@gmail.com>
2020-06-13 06:23:50 +0200

    utils folder

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// init environment variable
+// init environment variables
 const {
-    DB_NAME, DOMAIN_NAME, DB_DOMAIN_PORT, DB_APP_NAME, APP_PORT
+    DB_NAME, DB_DOMAIN_NAME, DB_DOMAIN_PORT, DB_APP_NAME, APP_PORT
 } = process.env
-
-const path = require('path')
 
 // connect to db
 const mongoose = require('mongoose')
 mongoose
-    .connect(`${DB_NAME}://${DOMAIN_NAME}:${DB_DOMAIN_PORT}/${DB_APP_NAME}`, { 
+    .connect(`${DB_NAME}://${DB_DOMAIN_NAME}:${DB_DOMAIN_PORT}/${DB_APP_NAME}`, { 
         useNewUrlParser: true,
         useUnifiedTopology: true 
     })
@@ -31,6 +29,7 @@ const express = require('express')
 const app = express()
 
 // get static routings with views files
+const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs')
--- utils/clean-docker.sh
+++ utils/clean-docker.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 docker container rm $(docker container ls -aq) -f
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 docker image rm baseapp_app -f
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+docker image rm astro_app -f
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 docker volume rm $(docker volume ls -q) -f
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

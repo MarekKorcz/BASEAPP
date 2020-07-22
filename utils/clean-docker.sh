67497fecf0dede97e46340cdd2bbae4217a1fd85
MarkKorcz <mark.korcz@gmail.com>
2020-07-22 12:46:30 +0200

    remove remoteapp container when fired

--- utils/clean-docker.sh
+++ utils/clean-docker.sh
@@ -5,6 +5,8 @@ docker container rm $(docker container ls -aq) -f
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 docker image rm baseapp_app -f
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+docker image rm remoteapp_app -f
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 docker image rm astro_app -f
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 docker volume rm $(docker volume ls -q) -f
